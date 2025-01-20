b576e13889afd0ca424a59c1b21f04f3a2c44873
RyanDePina <156648185+RyanDPC@users.noreply.github.com>
2025-01-20 11:08:14 +0100

    feat(db): connection to the db MiniGamesStore

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,97 +1,118 @@
-const crypto = require('crypto');
-const db = require('../db');  // Assure-toi que ce chemin est correct
+const crypto = require("crypto");
+const db = require("../db"); // Assure-toi que ce chemin est correct
 
 // Créer un utilisateur
 exports.create = async (req, res) => {
-    try {
-        const { username, email, password_hash, confirmPassword, admin } = req.body;
+  try {
+    const { username, email, password_hash, confirmPassword, admin } = req.body;
 
-        // Vérification que les champs ne sont pas undefined ou null
-        if (!password_hash || !confirmPassword) {
-            return res.status(400).json({ message: "Le mot de passe et la confirmation sont requis." });
-        }
+    // Vérification que les champs ne sont pas undefined ou null
+    if (!password_hash || !confirmPassword) {
+      return res
+        .status(400)
+        .json({ message: "Le mot de passe et la confirmation sont requis." });
+    }
 
-        // Vérifier si le mot de passe et la confirmation sont identiques
-        if (password_hash.trim() !== confirmPassword.trim()) {
-            return res.status(400).json({ message: "Les mots de passe ne correspondent pas." });
-        }
+    // Vérifier si le mot de passe et la confirmation sont identiques
+    if (password_hash.trim() !== confirmPassword.trim()) {
+      return res
+        .status(400)
+        .json({ message: "Les mots de passe ne correspondent pas." });
+    }
 
-        // Vérifier si le nom d'utilisateur ou l'email existe déjà
-        const userExists = await checkIfExists(username, email);
-        if (userExists) {
-            return res.status(400).json({ message: "Le nom d'utilisateur ou l'email est déjà pris." });
-        }
+    // Vérifier si le nom d'utilisateur ou l'email existe déjà
+    const userExists = await checkIfExists(username, email);
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ message: "Le nom d'utilisateur ou l'email est déjà pris." });
+    }
 
-        // Générer un sel pour le mot de passe
-        const salt = crypto.randomBytes(16).toString('hex');
-        const hashedPassword = hashPassword(password_hash, salt);
+    // Générer un sel pour le mot de passe
+    const salt = crypto.randomBytes(16).toString("hex");
+    const hashedPassword = hashPassword(password_hash, salt);
 
-        // Insérer l'utilisateur dans la base de données
-        const query = `
-            INSERT INTO Users (username, email, password_hash, salt, admin)
+    // Insérer l'utilisateur dans la base de données
+    const query = `
+            INSERT INTO users (username, email, password_hash, salt, admin)
             VALUES (?, ?, ?, ?, ?)
         `;
-        db.query(query, [username, email, hashedPassword, salt, admin], (err, results) => {
-            if (err) {
-                console.error('Erreur lors de l\'insertion de l\'utilisateur:', err);
-                return res.status(500).json({ message: "Erreur lors de l'inscription" });
-            }
-            return res.status(201).json({
-                id: results.insertId,
-                username,
-                email,
-                admin
-            });
+    db.query(
+      query,
+      [username, email, hashedPassword, salt, admin],
+      (err, results) => {
+        if (err) {
+          console.error("Erreur lors de l'insertion de l'utilisateur:", err);
+          return res
+            .status(500)
+            .json({ message: "Erreur lors de l'inscription" });
+        }
+        return res.status(201).json({
+          id: results.insertId,
+          username,
+          email,
+          admin,
         });
-
-    } catch (err) {
-        console.error('Erreur lors de l\'inscription:', err);
-        return res.status(500).json({ message: "Erreur lors de l'inscription" });
-    }
+      }
+    );
+  } catch (err) {
+    console.error("Erreur lors de l'inscription:", err);
+    return res.status(500).json({ message: "Erreur lors de l'inscription" });
+  }
 };
 
 // Connexion d'un utilisateur
 exports.login = async (req, res) => {
-    try {
-        const { username, password_hash } = req.body;
+  try {
+    const { username, password_hash } = req.body;
 
-        // Rechercher l'utilisateur dans la base de données
-        const query = `SELECT * FROM Users WHERE username = ? LIMIT 1`;
-        db.query(query, [username], (err, results) => {
-            if (err) return res.status(500).json({ message: "Erreur de connexion" });
-            if (results.length === 0) return res.status(400).json({ message: 'Utilisateur non trouvé' });
+    // Rechercher l'utilisateur dans la base de données
+    const query = `SELECT * FROM users WHERE username = ? LIMIT 1`;
+    db.query(query, [username], (err, results) => {
+      if (err) return res.status(500).json({ message: "Erreur de connexion" });
+      if (results.length === 0)
+        return res.status(400).json({ message: "Utilisateur non trouvé" });
 
-            const user = results[0];
-            const hashedPassword = hashPassword(password_hash, user.salt);
+      const user = results[0];
+      const hashedPassword = hashPassword(password_hash, user.salt);
 
-            if (hashedPassword === user.password_hash) {
-                req.session.user = { id: user.id, username: user.username, email: user.email, admin: user.admin };
-                return res.status(200).json({ id: user.id, username: user.username, email: user.email, admin: user.admin });
-            } else {
-                return res.status(400).json({ message: 'Mot de passe incorrect' });
-            }
+      if (hashedPassword === user.password_hash) {
+        req.session.user = {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          admin: user.admin,
+        };
+        return res.status(200).json({
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          admin: user.admin,
         });
-
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({ message: "Erreur de connexion" });
-    }
+      } else {
+        return res.status(400).json({ message: "Mot de passe incorrect" });
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Erreur de connexion" });
+  }
 };
 
 // Vérification si le nom d'utilisateur ou l'email existe déjà
 async function checkIfExists(username, email) {
-    return new Promise((resolve, reject) => {
-        const query = `SELECT * FROM Users WHERE username = ? OR email = ? LIMIT 1`;
-        db.query(query, [username, email], (err, results) => {
-            if (err) return reject(err);
-            resolve(results.length > 0);
-        });
+  return new Promise((resolve, reject) => {
+    const query = `SELECT * FROM users WHERE username = ? OR email = ? LIMIT 1`;
+    db.query(query, [username, email], (err, results) => {
+      if (err) return reject(err);
+      resolve(results.length > 0);
     });
+  });
 }
 
 // Fonction pour hacher le mot de passe avec le sel
 function hashPassword(password_hash, salt) {
-    const hash = crypto.createHash('sha256');
-    hash.update(password_hash + salt); // Appliquer le sel au mot de passe
-    return hash.digest('hex'); // Générer le hachage hexadécimal du mot de passe
+  const hash = crypto.createHash("sha256");
+  hash.update(password_hash + salt); // Appliquer le sel au mot de passe
+  return hash.digest("hex"); // Générer le hachage hexadécimal du mot de passe
 }
--- app/db.js
+++ app/db.js
@@ -1,15 +1,25 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // Configuration pour accéder à db_container sur le port exposé 6033
 const db = mysql.createPool({
-    host: 'db',         // Hôte pour le conteneur MySQL
-    port: 3306,         // Port exposé par Docker (6033)
-    user: 'Ryan',       // Nom d'utilisateur défini dans docker-compose
-    password: 'test',   // Mot de passe défini dans docker-compose
-    database: 'MyGameStore', // Nom de la base de données définie dans docker-compose
-    waitForConnections: true, // Attendre la connexion avant de refuser les demandes
-    connectionLimit: 10, // Nombre maximum de connexions simultanées dans le pool
-    queueLimit: 0        // Pas de limite de requêtes en attente
+  host: "localhost", // Hôte pour le conteneur MySQL
+  port: 6033, // Port exposé par Docker (6033)
+  user: "db_user", // Nom d'utilisateur défini dans docker-compose
+  password: "db_user_pass", // Mot de passe défini dans docker-compose
+  database: "MiniGamesStore", // Nom de la base de données définie dans docker-compose
+  waitForConnections: true, // Attendre la connexion avant de refuser les demandes
+  connectionLimit: 10, // Nombre maximum de connexions simultanées dans le pool
+  queueLimit: 0, // Pas de limite de requêtes en attente
+});
+
+// Test de la connexion à la base de données
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("Erreur de connexion à la base de données: ", err.message);
+  } else {
+    console.log("Connexion réussie à la base de données");
+    connection.release(); // Libérer la connexion une fois qu'elle n'est plus nécessaire
+  }
 });
 
 // Exporter le pool de connexions
